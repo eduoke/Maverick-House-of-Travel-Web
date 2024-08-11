a13457c35abbbd10f8b5aaa162f44bc9f55762e3
eduoke <edu_oke@proton.me>
2024-08-11 19:37:25 +0300

    Update cloudinary upload preset to fix missing uploads issue

--- app/components/inputs/ImageUpload.tsx
+++ app/components/inputs/ImageUpload.tsx
@@ -9,7 +9,7 @@ declare global {
   var cloudinary: any
 }
 
-const uploadPreset = "pgc9ehd5";
+const uploadPreset = "missdiana";
 
 interface ImageUploadProps {
   onChange: (value: string) => void;
